0a3a0e5fed750f6edf8444a641a4cbe44130a47a
Akito Kasai <a.kasai415@gmail.com>
2018-02-14 18:21:05 +0900

    foo

---
+++ app/controllers/random_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Random
+  def foo
+
+    
+  end
+
+  def banana
+    dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+  end
+end
