c03c8688de63f2f3c07da31c0480edc8884ea1ff
Akito Kasai <kasai@akito19.com>
2018-11-09 14:42:10 +0900

    many typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def defualt; end
+  def prevant; end
+  def centre; end
+  def theatre; end
+  def restorant; end
+  def dictionaly; end
 end
