9168e83e1eb59e94a0bb16b875b97d94dd024fdf
Akito Kasai <kasai@akito19.com>
2019-01-10 17:42:34 +0900

    duplicate methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
+  def duplicate_method; end
 end
